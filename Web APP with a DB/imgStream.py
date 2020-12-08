bab99dc66f536ac9f9bb205b532b2cbebab83f1d
Yi Tse Wu <tailer954@gmail.com>
2020-12-08 12:13:01 +0800

    Code on Raspberry Pi

--- Web APP with a DB/imgStream.py
+++ Web APP with a DB/imgStream.py
@@ -9,14 +9,16 @@ client = boto3.client('s3', region_name='us-west-1')
 time_min = 0
 
 while True:
+    # Ten minutes / one image
     if (time_min % 10 == 0) :
+        # Create invalidation
         os.system(''' aws cloudfront create-invalidation --distribution-id E3MMQ1PO9VN3I1 --paths "/imgStream.png" ''')
         # Take picture
         with picamera.PiCamera() as camera:
             camera.resolution = (1280, 720)
             camera.capture('/home/pi/imgStream.png')
         client.upload_file('/home/pi/imgStream.png', 'pestnodestream', "imgStream.png")
-        # print and make imgCount variable plus one
+        # Print the message 
         print("imgStream.png" + " is uploaded to S3 successfully!\n")
 
     # Count time
